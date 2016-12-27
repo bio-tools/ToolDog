b903e42b6ea40ee510b10e30e651b7973998c286
khillion <kenzo-hugo.hillion1@pasteur.fr>
2016-12-27 14:12:23 +0100

    Add end to end tests for 8 entries

---
+++ test/test_endtoend_tooldog.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+## Author(s): Kenzo-Hugo Hillion
+## Contact(s): kehillio@pasteur.fr
+## Python version: 3.5.2+
+## Creation : 12-19-2016
+
+'''
+Descrition
+'''
+
+###########  Import  ###########
+
+# General libraries
+import os
+import argparse
+import sys
+import filecmp
+import unittest
+from glob import glob
+
+# External libraries
+import requests_mock
+from nose_parameterized import parameterized
+
+# Class and Objects
+from tooldog import main
+
+###########  Constant(s)  ###########
+
+###########  Function(s)  ###########
+
+def get_test_files():
+    '''
+    List .json files of the test directory
+    '''
+    test_files = []
+    json_dir = os.path.dirname(__file__)
+    json_list = glob(os.path.join(json_dir, '*.json'))
+    for json_path in json_list:
+        name = os.path.splitext(json_path)[0]
+        xml_path = name + '.xml'
+        test_files.append([name, json_path, xml_path])
+    return test_files
+
+###########  Class(es)  ###########
+
+class TestToolDog(unittest.TestCase):
+
+    @parameterized.expand(get_test_files())
+    def test_from_local_to_galaxy(self, name, json_path, xml_path):
+        json = main.json_from_file(json_path)
+        biotool = main.json_to_biotool(json)
+        tmp_file = 'tmp_test_xml'
+        main.write_xml(biotool,tmp_file)
+        try:
+            self.assertTrue(filecmp.cmp(xml_path,tmp_file))
+        finally:
+            os.remove(tmp_file)
+
+    @parameterized.expand(get_test_files())
+    def test_from_biotools_to_galaxy(self, name, json_path, xml_path):
+        # Open json to be the content of the requests_mock
+        json_answer = main.json_from_file(json_path)
+        with requests_mock.mock() as m:
+            m.get('https://bio.tools/api/tool/' + name + '/version/1.0',\
+                  json=json_answer)
+            json = main.json_from_biotools(name, '1.0')
+            biotool = main.json_to_biotool(json)
+            tmp_file = 'tmp_test_xml'
+            main.write_xml(biotool,tmp_file)
+            try:
+                self.assertTrue(filecmp.cmp(xml_path,tmp_file))
+            finally:
+                os.remove(tmp_file)
+
+
+###########  Main  ###########
+
+if __name__ == "__main__":
+    unittest.main()
