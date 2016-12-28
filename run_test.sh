e3bb669daae64a3db958802d775fffa23b3d9ce8
khillion <kenzo-hugo.hillion1@pasteur.fr>
2016-12-28 11:10:10 +0100

    modified test to remove build success if one test fails

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,7 @@ LOGS=$(mktemp)
 nosetests --with-coverage --cover-package=tooldog 2> $LOGS
 cat $LOGS
 grep TOTAL $LOGS | awk '{ print "TOTAL: "$4; }'
+TEST=$(grep FAILED $LOGS)
 rm $LOGS
+if [[ ! -z $TEST ]]; then echo "Tests failed." && exit 1; fi
+exit 0
